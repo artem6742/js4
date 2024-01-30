741ecb943354ad5f38eaab542717f88d1a35276a
artem6742 <144213709+artem6742@users.noreply.github.com>
2024-01-30 10:56:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,157 @@
+
+// let obj1 = {}
+// let obj2 = new Object()
+// let key = prompt('enter key', 'adress')
+// let val = prompt(('Enter val'))
+// let user = {
+//     name: 'vasya',
+//     age: '15',
+//     isAdmin: false
+//     [key]: val
+// }
+// user.login = 'User1'
+// delete user.age
+// console.log(user)
+
+
+// function makeUser(name,age){
+//     return{name, age}
+// }
+// let user = makeUser('Vsya', 20)
+// console.log(user.name)
+
+
+
+// Task 1
+
+// const mas =  ['привет,','Mир', '!']
+// let a = mas[0] + mas[1] + mas[2]
+// console.log(a)
+
+// const mas =  ['привет,','Mир', '!']
+// let Task = mas[0] + mas[1] + mas[2]
+// console.log(a)
+
+// const mas =  ['привет,','Mир', '!']
+// mas[0] = 'пока'
+// console.log(mas)
+
+// let obj = {
+//     Петя : 500,
+//     Коля : 400,
+// }
+//
+// console.log('эзарплата Пети', obj.Петя )
+// console.log('эзарплата Пети', obj.Коля )
+//
+// let arr = {
+//     ru: ['Красный', 'Жёлтый', 'Зелёный'],
+//     en: ['sdfghjkl']
+// }
+// console.log(arr.ru[0])
+
+
+// Работа с массивами
+
+// arr = ['a', 'b', 'c']
+// alert(arr)
+
+// arr = ['a', 'b', 'c']
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+
+// arr = ['a', 'b', 'c', 'd']
+// console.log(arr[0] + '+' + arr[1] +', ' + arr[2] + '+' + arr[3])
+
+// arr = [2, 5, 3, 9]
+// console.log( arr[0] * arr[1] + arr[2] * arr[3])
+
+// var obj = {a:1, b:2, c:3}
+// console.log(obj.c)
+
+// var obj = {Петя:'1000', Коля:'500', Вася:'200'};
+// console.log(obj.Петя + obj.Вася)
+
+// const obj = {
+//     1:'Понедельник',
+//     2:'Вторник',
+//     3:'Среда',
+//     4:'Четверг',
+//     5:'Пятница',
+//     6:'Суббота',
+//     7:'Воскресенье'
+// }
+// console.log(obj["3"])
+
+
+// const obj = {
+//     1:'Понедельник',
+//     2:'Вторник',
+//     3:'Среда',
+//     4:'Четверг',
+//     5:'Пятница',
+//     6:'Суббота',
+//     7:'Воскресенье'
+// }
+// let day = 3
+// console.log(obj[day])
+
+// const mas = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+// console.log(mas[1][0])
+
+// const obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+// console.log(obj.js[0])
+
+
+// let arr = {
+//     ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+//     en: ['dfghjkl', 'fghjkl;','Sreda', 'hghjknjmk',  'fghjkl'],
+// }
+// console.log(arr.ru[0])
+// console.log(arr.en[2])
+
+
+let lang = 'ru'
+let day = 3
+let arr = {
+    lang: {
+        1:'Понедельник',
+        2:'Вторник',
+        3:'Среда',
+        4:'Четверг',
+        5:'Пятница',
+        6:'Суббота',
+        7:'Воскресенье'
+    },
+    en: ['dfghjkl', 'fghjkl;','Sreda', 'hghjknjmk',  'fghjkl'],
+}
+console.log(arr.lang[day])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
